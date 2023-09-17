964af9df80f9539f5f81e8a4848889f83decd45e
Tyuiu.MeshcheryakovPV@outlook.com <pavellol202@gmail.com>
2023-09-17 20:17:10 +0300

    Task6

---
+++ Tyuiu.MeshcheryakovPV.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MeshcheryakovPV.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.MeshcheryakovPV.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+                int[] numsArray = { 1, 2, 3, 4, 5 };
+
+                Console.WriteLine("Сумма элементов массива  = " + DataService.AdditionArray(numsArray));
+
+                Console.WriteLine("Сумма элементов массива  = " + DataService.SubtractionArray(numsArray));
+
+                Console.WriteLine("Сумма элементов массива  = " + DataService.MultiplicationArray(numsArray));
+
+                Console.ReadKey();
+        
+        }
+    }
+}
