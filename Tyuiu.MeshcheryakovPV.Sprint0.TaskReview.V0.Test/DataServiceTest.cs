cc20dce54471853c68a9f35f0b566f1bbd86592b
Tyuiu.MeshcheryakovPV@outlook.com <pavellol202@gmail.com>
2023-09-26 18:23:07 +0300

    TaskReview

---
+++ Tyuiu.MeshcheryakovPV.Sprint0.TaskReview.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MeshcheryakovPV.Sprint0.TaskReview.V0.Lib;
+namespace Tyuiu.MeshcheryakovPV.Sprint0.TaskReview.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalc()
+        {
+            Assert.AreEqual(DataService.Calc(1, 2, 3), 30);
+        }
+    }
+}
---
+++ Tyuiu.MeshcheryakovPV.Sprint0.TaskReview.V0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MeshcheryakovPV.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.MeshcheryakovPV.Sprint0.TaskReview.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение  переменной x: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение  переменной y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение  переменной z: ");
+            int z = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("(x + y + z)*5 = " + DataService.Calc(x, y, z));
+            Console.ReadKey();
+
+        }
+    }
+}
